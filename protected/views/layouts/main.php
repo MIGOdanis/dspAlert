3a86ecbc7e628e6db90733c6dc1f766adb5ccccd
MIGOdanis <j0sdarg06@gmail.com>
2015-03-10 09:31:49 +0800

    update file

---
+++ protected/views/layouts/main.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<!--[if lt IE 7]> <html class="lt-ie10 lt-ie9 lt-ie8 lt-ie7 ie6" lang="zh"> <![endif]-->
+<!--[if IE 7]> <html class="lt-ie10 lt-ie9 lt-ie8 ie7" lang="zh"> <![endif]-->
+<!--[if IE 8]> <html class="lt-ie10 lt-ie9 ie8" lang="zh"> <![endif]-->
+<!--[if IE 9]> <html class="lt-ie10 ie9" lang="zh"> <![endif]-->
+<!--[if gt IE 9]> <html class="gt-ie9" lang="zh"> <![endif]-->
+<!--[if !IE]><!--> <html class="modern" lang="zh"> <!--<![endif]-->
+	<head>
+	<meta http-equiv="X-UA-Compatible" content="IE=edge, chrome=1"> 
+	<meta charset="utf-8">
+	<title>YTB LOG Geter</title>
+	<!-- Common Style -->
+	<link href="/ytb/assets/bootstrap/css/bootstrap.min.css" rel="stylesheet" media="screen">
+	<link href="/ytb/assets/css/main.css" rel="stylesheet" media="screen">
+	</head>
+	<body>
+		<div id="main">
+			<?php echo $content; ?>
+		</div>
+	</body>
+</html>
